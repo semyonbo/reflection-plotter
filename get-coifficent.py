a4bb0b8d0cbb7c3a350ec27b2110a8d59cf7f076
semyonbo <semyon.borodulin@yandex.ru>
2022-12-18 01:22:37 +0300

    Some fixes

--- get-coifficent.py
+++ get-coifficent.py
@@ -1,10 +1,13 @@
-import numpy
+import numpy as np
+from numpy import arccos, arctan, pi
+from numpy.lib import emath
+
 
 def get_dielectic(lamb):
     with open('refractiveindex/esAG1.txt','r+') as file:
         lambs = list(map(lambda x: float(x), file.readline().split(',')))
         epsilons = list(map(lambda x: complex(x), file.readline().replace('i', 'j').split(',')))
-        epsilon=epsilons[0]
+        epsilon = epsilons[0]
         if len(lambs) == len(epsilons):
             for i in range(0, len(lambs)):
                 if lambs[i-1] <= lamb*(10**(-6)) <= lambs[i]:
@@ -15,7 +18,23 @@ def get_dielectic(lamb):
 
     return epsilon
 
-print(get_dielectic(0.1879))
+
+X=np.linspace(0.180,1.980,100)
+
+lamb = 0.35
+
+#for lamba in X:
+#    print(abs(emath.sqrt(get_dielectic(lamb) * 1) / (1 + get_dielectic(lamb))))
+
+print(get_dielectic(lamb))
+print(abs(emath.sqrt(get_dielectic(lamb)*1)/(1+get_dielectic(lamb))))
+print(abs(emath.sqrt(1 - (get_dielectic(lamb) / (1 + get_dielectic(lamb))))))
+theta0 = arccos(abs(emath.sqrt(get_dielectic(lamb)*1)/(1+get_dielectic(lamb))))
+
+theta1=arctan(abs(emath.sqrt(get_dielectic(lamb))))
+
+print(theta1*360/(2*pi))
+#theta0 = arccos(abs(emath.sqrt(1 - (get_dielectic(lamb) / (1 + get_dielectic(lamb))))))
 
 #-0.32404+2.5937i
 #1.879e-07
--- plotter.py
+++ plotter.py
@@ -1,4 +1,4 @@
-#P polatisation plotter
+# P polatisation plotter
 import matplotlib
 from numpy import arcsin, sin, sqrt, pi, cos, arctan, arccos
 import matplotlib.pyplot as plt
@@ -9,24 +9,28 @@ import os
 os.environ["PATH"] += os.pathsep + '/Library/TeX/texbin'
 
 
-def get_dielectic(lamb):
-    with open('refractiveindex/esAu.txt','r+') as file:
+def get_dielectic(lamb, material):
+    if material == 'Au':
+        mat = 'refractiveindex/esAu.txt'
+    else:
+        mat = 'refractiveindex/esAG1.txt'
+    with open(mat, 'r+') as file:
         lambs = list(map(lambda x: float(x), file.readline().split(',')))
         epsilons = list(map(lambda x: complex(x), file.readline().replace('i', 'j').split(',')))
         epsilon = epsilons[0]
         if len(lambs) == len(epsilons):
             for i in range(0, len(lambs)):
-                if lambs[i-1] <= lamb*(10**(-6)) <= lambs[i]:
-                    if (lambs[i-1] + abs((lambs[i] - lambs[i-1]) / 2)) >= lamb:
-                        epsilon = epsilons[i-1]
+                if lambs[i - 1] <= lamb * (10 ** (-6)) <= lambs[i]:
+                    if (lambs[i - 1] + abs((lambs[i] - lambs[i - 1]) / 2)) >= lamb:
+                        epsilon = epsilons[i - 1]
                     else:
                         epsilon = epsilons[i]
 
     return epsilon
 
 
-def calc_p_polaris_r(theta, lamb, k1x, type):
-    k1 = 2 * pi / lamb
+def calc_p_polaris_r(theta, lamb, k1x, type, mat):
+    k1 = 2 * pi / (lamb * (10 ** (-6)))
     if type == 'angle':
         k1x = k1 * sin(theta)
 
@@ -34,11 +38,11 @@ def calc_p_polaris_r(theta, lamb, k1x, type):
 
     k2x = k1x
 
-    k2 = emath.sqrt(get_dielectic(lamb)) * k1
+    k2 = emath.sqrt(get_dielectic(lamb, mat)) * k1
 
     k2z = emath.sqrt(k2 ** 2 - k2x ** 2)
 
-    rp = (1 * k2z - get_dielectic(lamb) * k1z) / (get_dielectic(lamb) * k1z + 1 * k2z)
+    rp = (-1 * k2z + get_dielectic(lamb, mat) * k1z) / (get_dielectic(lamb, mat) * k1z + 1 * k2z)
 
     return rp
 
@@ -48,21 +52,23 @@ plt.rcParams['text.usetex'] = True
 # Lambda linspace - plotting graph |R_p^2| (lambda)
 Y = np.linspace(0.1879, 1.937, 100)
 
-#Put angle here:
-angle = pi / 4
-
+# Put angle here:
+angle = 30 * 2 * pi / 360
 
 angle_deg = round(angle * 360 / (2 * pi), 0)
 
 fig2 = plt.figure(2)
 ax2 = plt.gca()
 
-R_S = []
+R_S_Au = []
+R_S_Ag = []
 
 for i in Y:
-    R_S.append(emath.sqrt(calc_p_polaris_r(angle, i, 0, 'angle') * np.conj(calc_p_polaris_r(angle, i, 0, 'angle'))))
+    R_S_Au.append(np.abs(calc_p_polaris_r(angle, i, 0, 'angle', 'Au')**2))
+    R_S_Ag.append(np.abs(calc_p_polaris_r(angle, i, 0, 'angle', 'Ag')**2))
 
-plt.plot(Y, np.real(R_S), label=r'$|R_{p}^{2}|$')
+plt.plot(Y, R_S_Au, label=r'$|R_{p}^{2}|$ for Au', color='forestgreen', lw='1.5')
+plt.plot(Y, R_S_Ag, label=r'$|R_{p}^{2}|$ for Ag', color='firebrick', lw='1.5')
 ax2.set_xlim(xmin=0)
 # ax2.set_ylim(ymin=0)
 ax2.set_xlabel(r'$\lambda$ ($\mu m$)')
@@ -70,34 +76,35 @@ plt.title(r"Dependence $|R_{p}^{2}|$ from $\lambda$ for " + fr"$\theta_1={angle_
 
 plt.legend()
 plt.grid()
-plt.savefig("Plot2.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("Plot_Rp(lamb).pdf", format="pdf", bbox_inches="tight")
 plt.show()
 
 
-#Plotting graph |R_p^2| (angle) for lamda:
+# Plotting graph |R_p^2| (angle):
 # Angle linspace
 X = np.linspace(0, pi / 2, 100)
 
-#Put lambda here (in micrometre)
-lamb = 0.6
-
-theta0 = arccos(abs(emath.sqrt(1 - (get_dielectic(lamb) / (1 + get_dielectic(lamb))))))
-
-theta0_deg = round(theta0 * 360 / (2 * pi), 0)
+# Put lambda here (in micrometre)
+lamb_Au = 0.6
+lamb_Ag = 0.35
 
 fig1 = plt.figure(1)
 
 ax1 = plt.gca()
 
-plt.plot(X, np.real(emath.sqrt(calc_p_polaris_r(X, lamb, 0, 'angle') * np.conj(calc_p_polaris_r(X, lamb, 0, 'angle')))),
-         label=r'$|R_{p}^{2}|$')
-plt.axvline(theta0, color='red', lw=1, label=fr'Resonance angle: ${theta0_deg}^\circ$ ')
+plt.plot(X, np.abs(calc_p_polaris_r(X, lamb_Au, 0, 'angle', 'Au')**2), label=r'$|R_{p}^{2}|$ for Au:'+fr"$\lambda={lamb_Au} \;\mu m$", color='firebrick', lw='1.5')
+
+plt.plot(X, np.abs(calc_p_polaris_r(X, lamb_Ag, 0, 'angle', 'Ag')**2), label=r'$|R_{p}^{2}|$ for Au:'+fr"$\lambda={lamb_Ag} \;\mu m$", color='forestgreen', lw='1.5')
+
+
 plt.legend()
 plt.grid()
-plt.title(r"Dependence $|R_{p}^{2}|$ from $\theta_{1}$ for " + fr"$\lambda={lamb} \;\mu m$")
+plt.title(r"Dependence $|R_{p}^{2}|$ from $\theta_{1}$")
+
+# plt.title(r"Dependence $Im(R_{p})$ from $\theta_{1}$ for " + fr"$\lambda={lamb} \;\mu m$")
 ax1.set_xlabel(r'Angle (rad)')
 
-plt.savefig("Plot1.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("Plot_abs(Rp**2)(angle).pdf", format="pdf", bbox_inches="tight")
 
 ax1.set_xlim(xmin=0)
 # ax1.set_ylim(ymin=0)
@@ -105,45 +112,112 @@ ax1.set_xlim(xmin=0)
 # plt.xticks(plt.xticks()[0],[r"$" + format(r/np.pi, ".2g")+ r"\pi$" for r in plt.xticks()[0]])
 plt.show()
 
+# Plotting graph Im(R_p)(angle) for lamda Au and Ag:
+fig4 = plt.figure(4)
+ax4 = plt.gca()
+
+plt.plot(X, np.imag(calc_p_polaris_r(X, lamb_Au, 0, 'angle', 'Au')), label=r'$Im(R_{p})$ for Au:'+fr"$\lambda={lamb_Au} \;\mu m$", color='firebrick', lw='1.5')
+plt.plot(X, np.imag(calc_p_polaris_r(X, lamb_Ag, 0, 'angle', 'Ag')), label=r'$Im(R_{p})$ for Ag:'+fr"$\lambda={lamb_Ag} \;\mu m$", color='forestgreen', lw='1.5')
+
+plt.legend()
+plt.grid()
+plt.title(r"Dependence $Im(R_{p})$ from $\theta_{1}$")
+ax4.set_xlabel(r'Angle (rad)')
+
+plt.savefig("Plot_Im(Rp)(angle).pdf", format="pdf", bbox_inches="tight")
+
+ax4.set_xlim(xmin=0)
+
+plt.show()
 
 # Plot graph |R_p^2| from Array of k_x
-#kx array starts from: a*k1
-a=0
+# kx array starts from: a*k1
+a = 0
 
-#kx array ends with: b*k1
-b=5
+# kx array ends with: b*k1
+b = 4
 
-#Amount of dots between a and b:
-resolution=200
+# Amount of dots between a and b:
+resolution = 200
 
-#Lambda of wave:
-lamb = 0.6
+# Lambda of wave:
+lamb = 0.350
 
 X1 = np.linspace(a, b, resolution)
 fig3 = plt.figure(3)
 ax3 = plt.gca()
 angle = 0
-k1 = 2 * pi / lamb
+k1 = 2 * pi / (lamb * 10 ** (-6))
 X = X1 * k1
 
-kx_crit = (2 * pi / lamb) * emath.sqrt(1 * get_dielectic(lamb) / (1 + get_dielectic(lamb)))
+kx_crit = (2 * pi / (lamb * 10 ** (-6))) * emath.sqrt(1 * get_dielectic(lamb,'Ag') / (1 + get_dielectic(lamb,'Ag')))
 
-plt.plot(X, np.real(emath.sqrt(calc_p_polaris_r(angle, lamb, X, 'None') * np.conj(calc_p_polaris_r(angle, lamb, X, 'None')))),
-         label=r'$|R_{p}^{2}|$')
-plt.axvline(np.real(kx_crit), color='red',linestyle='dotted', lw=1, label=r'Critical $k_{1x}$:'+fr' ${round(np.real(kx_crit/k1), 3)} \cdot k_{1}$ ')
+plt.plot(X, np.real(
+    emath.sqrt(calc_p_polaris_r(angle, lamb, X, 'None','Ag') * np.conj(calc_p_polaris_r(angle, lamb, X, 'None','Ag')))),
+         label=r'$|R_{p}^{2}|$ for Ag', color='midnightblue', lw=1.5)
+plt.axvline(np.real(kx_crit), color='firebrick', linestyle='-', lw=1.5,
+            label=r'Critical $k_{1x}$:' + fr' ${round(np.real(kx_crit / k1), 3)} \cdot k_{1}$ ')
 plt.legend()
 plt.grid()
 plt.title(
     r"Dependence $|R_{p}^{2}|$ from $k_{1x}$ for " + fr"$\lambda={lamb} \;\mu m$" + r" where $k_1=\frac{2 \pi}{\lambda}$")
 ax3.set_xlabel(r'$k_{1x}$')
 ax3.set_xlim(xmin=0)
-plt.hlines(y = 0, xmin = 0, xmax = k1, color='g', linestyle='-',linewidth=1.5)
-plt.hlines(y = 0, xmin = k1, xmax=5*k1, color='r', linestyle='-',linewidth=1.5)
+plt.hlines(y=0, xmin=0, xmax=k1, color='g', linestyle='-', linewidth=1.5)
+plt.hlines(y=0, xmin=k1, xmax=5 * k1, color='r', linestyle='-', linewidth=1.5)
 # ax2.set_ylim(ymin=0)
 
-plt.xticks(np.arange(0, b*k1+1, k1))
-ax3.text(1, -0.75, 'propagating', style='italic')
-ax3.text(4.1*k1, -0.75, 'evanescent', style='italic')
+plt.xticks(np.arange(0, b * k1 + 1, k1))
+ax3.text(0.15 * k1, -0.5, 'propagating', style='italic')
+ax3.text(3.1 * k1, -0.5, 'evanescent', style='italic')
 plt.xticks(plt.xticks()[0], [r"$" + format(r / k1, ".2g") + r"k_1$" for r in plt.xticks()[0]])
-plt.savefig("Plot3.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("Plot_abs(Rp**2)_evanicent_Ag.pdf", format="pdf", bbox_inches="tight")
 plt.show()
+
+
+
+# # Lambda of wave - plotting compramation betwen two types of materials:
+# lamb = 0.600
+# a=0.85
+# b=1.35
+#
+# X1 = np.linspace(a, b, resolution)
+# fig5 = plt.figure(5)
+# ax5 = plt.gca()
+# angle = 0
+# k1 = 2 * pi / (lamb * 10 ** (-6))
+# X = X1 * k1
+#
+# kx_crit_Au = (2 * pi / (lamb * 10 ** (-6))) * emath.sqrt(1 * get_dielectic(lamb,'Au') / (1 + get_dielectic(lamb,'Au')))
+#
+# kx_crit_Ag = (2 * pi / (lamb * 10 ** (-6))) * emath.sqrt(1 * get_dielectic(lamb,'Ag') / (1 + get_dielectic(lamb,'Ag')))
+#
+#
+# plt.plot(X, np.real(
+#     emath.sqrt(calc_p_polaris_r(angle, lamb, X, 'None','Au') * np.conj(calc_p_polaris_r(angle, lamb, X, 'None','Au')))),
+#          label=r'$|R_{p}^{2}|$ for Au', color='firebrick', lw=1.5)
+# plt.axvline(np.real(kx_crit_Au), color='royalblue', linestyle='dashed', lw=1.5,
+#             label=r'Crit $k_{1x}$ for Au:' + fr' ${round(np.real(kx_crit_Au / k1), 3)} \cdot k_{1}$ ')
+#
+# plt.plot(X, np.real(
+#     emath.sqrt(calc_p_polaris_r(angle, lamb, X, 'None','Ag') * np.conj(calc_p_polaris_r(angle, lamb, X, 'None','Ag')))),
+#          label=r'$|R_{p}^{2}|$ for Ag', color='forestgreen', lw=1.5)
+# plt.axvline(np.real(kx_crit_Ag), color='darkorange', linestyle='dashed', lw=1.5,
+#             label=r'Crit $k_{1x}$ for Ag:' + fr' ${round(np.real(kx_crit_Ag/ k1), 3)} \cdot k_{1}$ ')
+#
+# plt.legend()
+# plt.grid()
+# plt.title(
+#     r"Dependence $|R_{p}^{2}|$ from $k_{1x}$ for " + fr"$\lambda={lamb} \;\mu m$" + r" where $k_1=\frac{2 \pi}{\lambda}$")
+# ax5.set_xlabel(r'$k_{1x}$')
+# ax5.set_xlim(xmin=a*k1)
+# plt.hlines(y=0, xmin=a*k1, xmax=k1, color='g', linestyle='-', linewidth=1.5)
+# plt.hlines(y=0, xmin=k1, xmax=b*k1, color='r', linestyle='-', linewidth=1.5)
+# ax5.set_ylim(ymax=30, ymin=-2)
+#
+# plt.xticks(np.arange(a*k1, b * k1 + 1, k1/2))
+# ax5.text((a+0.01) * k1, -1.2, 'propagating', style='italic')
+# ax5.text(1.26 * k1, -1.2, 'evanescent', style='italic')
+# plt.xticks(plt.xticks()[0], [r"$" + format(r / k1, ".2g") + r"k_1$" for r in plt.xticks()[0]])
+# plt.savefig("Plot_abs(Rp**2)_evanicent.pdf", format="pdf", bbox_inches="tight")
+# plt.show()
